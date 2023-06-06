a8641b81dc7cd60a59d71361af41fffe0a0e9df5
Malik Yousaf <malikyousaf.live@gmail.com>
2023-06-05 23:14:55 -0700

    portfolio

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-none ">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-center"
+            id="navbarSupportedContent"
+          >
+            {/* me-auto mb-2 mb-lg-0 */}
+            <ul className="navbar-nav ">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Instagram
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
---
+++ src/content/hero/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+export default function Hero() {
+  return (
+    <>
+      <div className="hero">
+        <Navbar />
+      </div>
+      <div className="blur"></div>
+    </>
+  );
+}
